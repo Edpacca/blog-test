c2cadb7d37b6b345f0c50fd81d12a8362a8c6566
Eddie Pace <epace@scottlogic.com>
2022-05-11 13:37:25 +0100

    set up identity scripts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,10 @@ import React from "react";
 import Header from '../components/Header';
 import PostLink from '../components/PostLink';
 import logo from '../assets/EP-logo.svg'
+import netlifyIdentity from 'netlify-identity-widget';
 import { graphql } from "gatsby"
+
+netlifyIdentity.init({});
 
 const Home = ({data: {allMarkdownRemark: { edges }}, }) => {
   
